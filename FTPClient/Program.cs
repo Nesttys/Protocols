6e9e664d25d9d0423cd4ad365d3d16bf1e302dcf
Student <student@lan.itstep.kiev.ua>
2018-11-27 09:57:24 +0200

    Protocols Project

---
+++ FTPClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string endPoint = "https://giphy.com/gifs/Sk3KytuxDQJQ4";
+            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(endPoint);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream ftpStream = response.GetResponseStream();
+            Stream fileStream = File.Create("ff.gif");
+            byte[] buffer = new byte[1024];
+            int read;
+            int total = 0;
+            while((read = ftpStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fileStream.Write(buffer, 0, read);
+                total += read;
+                Console.WriteLine($"Downloaded {total} bytes");
+            }
+            ftpStream.Close();
+            fileStream.Close();
+            response.Close();
+        }
+    }
+}
---
+++ SMTPProtocol/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace SMTPProtocol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static MailMessage CreateMessage(string from, string to, string subject, string body, string filePath = null)
+        {
+            MailMessage message = new MailMessage(from, to, subject, body);
+            if (!string.IsNullOrEmpty(filePath))
+                message.Attachments.Add(new Attachment(filePath));
+            return message;
+        }
+
+        static void Send(string host, int port, string password, MailMessage message)
+        {
+            SmtpClient client = new SmtpClient(host, port);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(message.From.Address, password);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Send(message);
+            message.Dispose();
+            client.Dispose();
+
+        }
+    }
+}
